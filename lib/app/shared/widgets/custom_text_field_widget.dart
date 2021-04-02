c0f865e3d3b4cb3fd0c0e9ce06810a61080039a3
williambz12 <williamsilva754@gmail.com>
2021-04-02 16:15:06 -0300

    refactor textfield

--- lib/app/shared/widgets/custom_text_field_widget.dart
+++ lib/app/shared/widgets/custom_text_field_widget.dart
@@ -1,22 +1,14 @@
-import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextFieldWidget extends StatefulWidget {
   final String hintText;
-  final TextInputType keyboardType;
   final TextEditingController controller;
   final Widget suffixIcon;
-  final void Function(String) onChanged;
-
-  final void Function(String) onValidator;
 
   CustomTextFieldWidget({
-    this.hintText,
-    this.keyboardType,
     @required this.controller,
+    this.hintText,
     this.suffixIcon,
-    this.onChanged,
-    this.onValidator,
   });
 
   @override
@@ -27,19 +19,11 @@ class _CustomTextFieldWidgetState extends State<CustomTextFieldWidget> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      keyboardType: widget?.keyboardType,
       controller: widget?.controller,
-      onChanged: widget?.onChanged,
-      validator: widget.onValidator,
       maxLines: 1,
       decoration: InputDecoration(
         labelText: widget.hintText,
-        contentPadding: EdgeInsets.only(
-          left: 0,
-          right: 0,
-          top: 15,
-          bottom: 15,
-        ),
+        contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 0),
         suffixIcon: widget?.suffixIcon,
       ),
     );
